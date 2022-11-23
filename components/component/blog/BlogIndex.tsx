8986e528b1cb28132635e059979384361989cea3
CaptainCoder <78704276+captainplanto@users.noreply.github.com>
2022-11-23 01:54:50 +0100

    updated hom page

---
+++ components/component/blog/BlogIndex.tsx
@@ -0,0 +1,12 @@
+import { BlogComponent } from "../../cards/Blog.component";
+import { ContactComponent } from "../../common/Contact.component";
+
+export const BlogIndex = () => {
+
+  return (
+    <div>
+      <BlogComponent />
+      <ContactComponent />
+    </div>
+  );
+};
--- components/component/home/Home.index.tsx
+++ components/component/home/Home.index.tsx
@@ -14,7 +14,7 @@ import { NavBarComponent } from "../nav/Navbar.component";
 import Head from "next/head";
 import { IIcon } from "../../../types/types";
 import { ContactComponent } from "../../common/Contact.component";
-import { ServicesComponent } from "../services/Services.component";
+
 
 export const HomeIndex = ({ icons }: { icons: IIcon[] }) => {
   return (
@@ -25,17 +25,14 @@ export const HomeIndex = ({ icons }: { icons: IIcon[] }) => {
         <meta name="copyright" content={copyright} />
         <meta name="language" content="EN" />
         <meta name="Classification" content="website" />
-        <meta
-          name="author"
-          content="Anthony Awoniyi, tonymarketing@yahoo.com"
-        />
-        <meta name="url" content="" />
+        <meta name="author" content="Anthony Awoniyi, tonymarketing@yahoo.com"/>
+        <meta name="url" content="https://portfolio-anthony.vercel.app" />
         <meta
           property="og:description"
           content="Frontend Engineer, Expert in developing web application using modern tech stack. React, Next js developer"
         />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="" />
+        <meta property="og:url" content="https://portfolio-anthony.vercel.app" />
         <link
           rel="icon"
           href="https://res.cloudinary.com/captainkoder/image/upload/v1668103284/portfolio/Portfolio-image_cbrpej.png"
@@ -45,9 +42,6 @@ export const HomeIndex = ({ icons }: { icons: IIcon[] }) => {
       <LayOutComponent>
         <HeaderComponent />
         <AboutComponent />
-      </LayOutComponent>
-      <ServicesComponent />
-      <LayOutComponent>
         <StackComponent icons={icons} />
         <SectionComponent title={title} />
       </LayOutComponent>
@@ -56,3 +50,7 @@ export const HomeIndex = ({ icons }: { icons: IIcon[] }) => {
     </>
   );
 };
+
+
+
+
